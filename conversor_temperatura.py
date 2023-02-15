91ea883bb825934da9135167fe244306387a8a4a
dannyardila <dannyardila059@gmail.com>
2023-02-15 07:53:57 -0500

    se agrago el programa

--- conversor_temperatura.py
+++ conversor_temperatura.py
@@ -1,2 +1,19 @@
 # programa para convertir una cantidad a grados centigrados a su equivalencia en Farenheit y kelvin
 
+print("-------------------------------------------")
+print("-------Valor De Grados Centigrados --------")
+print("-------------------------------------------")
+
+# input
+c = int (input ("digite el valor de c:"))
+
+# processing
+F =  ( c * 1.8 + 32 )
+k = (c + 273.15)
+
+# output
+print("-------------------------------------------")
+print("---------------RESULTADOS------------------")
+print("-------------------------------------------")
+print ( "la coversion en farenhell en" + str (F))
+print ( "la conversion de kelvin es" + str (k))
